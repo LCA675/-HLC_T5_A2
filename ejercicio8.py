fea3c642dd5086498adf4f90fb56ece66e6da5d9
LCA675 <lauracalleanguita@gmail.com>
2025-01-23 10:07:39 +0100

    Ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,9 @@
+def exponente():
+    numero=int(input("Introduza un número ENTERO y POSITIVO: "))
+    cuadrado=[]
+    for i in range(1, numero+1):
+        cuadrado.append(i**2)
+
+    print(cuadrado)  
+
+exponente()
