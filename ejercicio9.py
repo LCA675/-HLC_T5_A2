e418d11f23f5e9784fc580a4fea87e5ed239a61b
LCA675 <lauracalleanguita@gmail.com>
2025-01-24 11:05:35 +0100

    Fin del boletin 2

--- ejercicio9.py
+++ ejercicio9.py
@@ -7,6 +7,6 @@ while num!=numero:
         print("El número a adivinar es menor")
     else:
         print("El número a adivinar es mayor")
-    num=int(input("Adivina un número entre el 1 y el 50: "))
+    num=int(input("Vuelve a intentarlo: "))
 print("Ha adivinado el número!!!")
     
