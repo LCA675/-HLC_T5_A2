f4cda9b322435f2396c089441545d9d9c898f381
LCA675 <lauracalleanguita@gmail.com>
2025-01-24 11:03:03 +0100

    Ejercicio 9

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,7 +1,12 @@
-num=(int(input("Adivina un número entre el 1 y el 50")))
+num=(int(input("Adivina un número entre el 1 y el 50: ")))
 import random
 numero = random.randint(1, 50)
-
-if (num>numero):
-    pri
+while num!=numero:
+    print("Número incorrecto, vuelva a intentarlo")
+    if (num>numero):
+        print("El número a adivinar es menor")
+    else:
+        print("El número a adivinar es mayor")
+    num=int(input("Adivina un número entre el 1 y el 50: "))
+print("Ha adivinado el número!!!")
     
